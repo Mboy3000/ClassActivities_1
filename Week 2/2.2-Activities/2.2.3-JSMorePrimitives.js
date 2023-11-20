3b7359d7fb3c4dfb9b9f0a8c7757136472ea9c71
Your Name <you@example.com>
2023-11-20 13:36:26 -0600

    update
    update

---
+++ Week 2/2.2-Activities/2.2.3-JSMorePrimitives.js
@@ -0,0 +1,23 @@
+// Create a variable "timeOfDay" and assign it a string
+const timeOfDay="string";
+// Create a variable "greeting" that references a template literal
+// Use "timeOfDay" in the template literal to create a message like "Good morning!" or "Good evening!"
+const greeting ='good morning';
+// Print "greeting"
+console.log(greeting);
+// Create a new variable, but do not assign it a value
+const variable ="";
+// Print the new variable and its type
+// What type should we expect? false/ undifiend
+console.log(variable);
+// Assign the variable a value that indicates the variable is purposely blank
+// What value should we assign?
+variable=3;
+// Print the variable and its type again
+// What type should we expect?
+console.log(variable);
+// Try to print a variable that does not exist
+// What should we expect to print in the CLI?
+console.log(greeting);
+// Print "greeting" again
+// Will this line run?
